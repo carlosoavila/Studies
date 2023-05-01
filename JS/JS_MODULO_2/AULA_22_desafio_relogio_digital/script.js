99da4cac9b3ddd420970218410544a1671fd5e2b
Carlos ALberto <carlosaoavila1221@gmail.com>
2023-05-01 15:49:14 -0300

    feat: new clock

--- JS/JS_MODULO_2/AULA_22_desafio_relogio_digital/script.js
+++ JS/JS_MODULO_2/AULA_22_desafio_relogio_digital/script.js
@@ -7,9 +7,9 @@ function exibirRelogio() {
     let hora = String(tempo.getHours()).padStart(2, "0")
     let minuto = String(tempo.getMinutes()).padStart(2, "0")
     let segundo = String(tempo.getSeconds()).padStart(2, "0")
-    let horaAtual
+    let horaAtual 
     if(hora > 12 ) {
-       hora -= hora - 1 
+       hora - 12 
        horaAtual = hora + ":" + minuto + ":" + segundo + " PM"
     } else {
         horaAtual = hora + ":" + minuto + ":" + segundo + " AM"
