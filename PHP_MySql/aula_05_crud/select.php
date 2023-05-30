7af65dc359a8b9ec564e30dfb39cc77de746ee5b
Carlos ALberto <carlosaoavila1221@gmail.com>
2023-05-30 06:29:40 -0300

    new

--- PHP_MySql/aula_05_crud/select.php
+++ PHP_MySql/aula_05_crud/select.php
@@ -5,6 +5,19 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Lista de produtos cadastrados</title>
+  <style>
+
+    table, th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+      padding: 10px;
+    }
+
+    table {
+      width: 90%;
+    }
+
+  </style>
 </head>
 <body>
   <h2>Lista de produtos cadastrados</h2>
@@ -17,7 +30,45 @@
     $dadosProdutos = $conn->query($sql);
     //Verificando se foi retornado alguem registro
     if($dadosProdutos->num_rows > 0) {
-      echo("Foi retornado algum registro...");
+  ?>
+
+  <table>
+    <tr>
+      <th>Id</th>
+      <th>Nome do produto</th>
+      <th>Categoria do produto</th>
+      <th>Descrição do produto</th>
+    </tr>
+  
+<?php
+     while($exibir = $dadosProdutos->fetch_assoc()) {
+?>
+    <tr>
+      <td><?php echo ($exibir["idProduto"]); ?> </td>
+      <td><?php echo ($exibir["nmProduto"]); ?> </td>
+      <?php 
+
+        $sqlCategoria = "SELECT * FROM tbcategoria WHERE idCategoria  = " . $exibir["idCategoria"];
+        $dadosCategoria = $conn->query($sqlCategoria);
+        $exibirCatergoria = $dadosCategoria->fetch_assoc();
+      
+      ?>
+      
+      
+      <td><?php echo ($exibirCatergoria["nmCategoria"]); ?> </td>
+      <td><?php echo ($exibir["descProduto"]); ?> </td>
+    </tr>      
+
+
+
+<?php
+      echo("id:" .  $exibir['idProduto'] . " - Produto: " . $exibir['nmProduto'] . " - " . $exibir['idCategoria'] . "<br>");
+      }
+?>
+  </table>
+
+  <?php
+
     }else {
       echo("Nenhum registro foi encontrado");
     }
